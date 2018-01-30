8e7ebd1fb192bbdb20570ad09818064b336e02d9
Marowins <gamemaro@naver.com>
2018-01-30 23:18:43 +0900

    Project reset & Frame setting

---
+++ Life Of Miner/Assets/OreAnimController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OreAnimController : MonoBehaviour {
+
+	// animator require;
+	Animator anim;
+
+	void Start() {
+		anim = GetComponent<Animator> ();
+	}
+		
+	void Update() {
+		
+	}
+
+}
---
+++ Life Of Miner/Assets/TouchManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour {
+
+	// script static화
+	public static TouchManager instance;
+
+	// ore & player animation controllor
+
+	public bool jud;
+
+	void Awake() {
+		instance = this;
+	}
+
+	// function value reset
+	void Start() {
+		
+	}
+
+	void Update() {
+		
+	}
+
+}
