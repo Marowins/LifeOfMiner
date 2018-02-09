2135553c9f8d5155692d119895cb7a43d84a259e
Marowins <gamemaro@naver.com>
2018-02-09 23:26:02 +0900

    Anim bug fix

---
+++ Life Of Miner/Assets/Script/TouchManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour {
+
+	// require Animator
+	public Animator Ore;
+
+	// jud variable
+	public bool jud = true;
+
+	void Update() {
+		if (Input.GetButtonDown ("Fire1")) {
+			if (jud) {
+				Ore.SetInteger ("JUDGE", 1);
+				jud = false;
+			} else {
+				Ore.SetInteger ("JUDGE", -1);
+				jud = true;
+			}
+		} else {
+			Ore.SetInteger ("JUDGE", 0);
+		}
+	}
+
+}
